336b8a5aa7226f874bbcd346ecefb969442f6921
Michał Kulik <kulikkmichal@icloud.com>
2026-01-20 21:32:28 +0100

    climb

---
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.ElevatorConfig;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
+
+public class ClimbSubsystem extends SubsystemBase {
+    private final SmartMotorControllerConfig config = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withMechanismCircumference(Meters.of(Inches.of(0.25).in(Meters) * 22))
+        .withIdleMode(MotorMode.BRAKE)
+        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+        .withMomentOfInertia(Inches.of(0.5), Pounds.of(2))
+        .withClosedLoopController(4, 0, 0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
+        .withSimClosedLoopController(4, 0, 0, MetersPerSecond.of(0.5), MetersPerSecondPerSecond.of(0.5))
+        .withFeedforward(new SimpleMotorFeedforward(0, 0 ,0))
+        .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+        .withMotorInverted(false)
+        .withTelemetry("ClimbMotor", TelemetryVerbosity.HIGH);
+
+    private final SmartMotorController leftMotor = new SparkWrapper(
+        new SparkMax(10, MotorType.kBrushless), DCMotor.getNEO(1), config);
+
+    private final SmartMotorController rightMotor = new SparkWrapper(
+        new SparkMax(11, MotorType.kBrushless), DCMotor.getNEO(1), config);
+
+    ElevatorConfig elevConfig = new ElevatorConfig(leftMotor)
+        .withStartingHeight(Meters.of(0.5))
+        .withHardLimits(Meters.of(0), Meters.of(2.0))
+        .withTelemetry("Climbing", TelemetryVerbosity.HIGH)
+        .withMass(Pounds.of(10));
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,36 +1,61 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.local.SparkWrapper;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class ShooterSubsystem extends SubsystemBase {
-    public Talon shooterMaster;
-    public Talon shooterSlave;
+    private SmartMotorControllerConfig rightWheelConfig = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withIdleMode(MotorMode.COAST)
+        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+        .withMomentOfInertia(Inches.of(4), Pounds.of(2))
+        .withClosedLoopController(1, 0, 0)
+        .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+        .withMotorInverted(false)
+        .withTelemetry("RightFlyWheel", SmartMotorControllerConfig.TelemetryVerbosity.HIGH);
+
+    private SmartMotorController rightWheel = new SparkWrapper(
+        new SparkMax(4, MotorType.kBrushless), DCMotor.getNEO(1), rightWheelConfig);
+
+    private SmartMotorControllerConfig leftWheelConfig = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withIdleMode(MotorMode.COAST)
+        .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+        .withMomentOfInertia(Inches.of(4), Pounds.of(2))
+        .withClosedLoopController(1, 0, 0)
+        .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+        .withMotorInverted(false)
+        .withTelemetry("LeftFlyWheel", SmartMotorControllerConfig.TelemetryVerbosity.HIGH);
+
+    private SmartMotorController leftWheel = new SparkWrapper(
+        new SparkMax(5, MotorType.kBrushless),  DCMotor.getNEO(1), leftWheelConfig);
 
     public ShooterSubsystem() {
-        shooterMaster = new Talon(0);
-        shooterSlave = new Talon(1);
-
-        shooterMaster.setInverted(false);
-        shooterSlave.setInverted(true);
+        rightWheel.setupTelemetry();
+        leftWheel.setupTelemetry();
     }
 
-    public void setShooterPower(double power) {
-        shooterMaster.set(power);
-        shooterSlave.set(power);
+    public Pair<LinearVelocity, LinearVelocity> getLinearSpeed() {
+        return Pair.of(rightWheel.getMeasurementVelocity(), leftWheel.getMeasurementVelocity());
     }
 
-    public void stopShooter() {
-        shooterMaster.stopMotor();
-        shooterSlave.stopMotor();
-    }
 
-    public double getIntakePower() {
-        return shooterMaster.get();
-    }
-
-    public void setShooterVolts(double volts) {
-        shooterMaster.setVoltage(volts);
-        shooterSlave.setVoltage(volts);
-    }
 }
